c8d891e63549170b265865cb66920800e62e8b36
GiorgiPasieshvili <giopasieshvili2@gmail.com>
2022-11-14 05:46:38 +0400

    Add header styles and lang options

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,36 +1,47 @@
+import styles from "@/styles/Header.module.scss";
 import Link from "next/link";
 
 export default function Header() {
   return (
-    <div id="header" className="header">
-      Header Section
-      <nav>
-        <ul>
-          <li>
-            <Link href="/">home</Link>
-          </li>
-          <li>
-            <Link href="/#projects" scroll={false}>
-              projects
-            </Link>
-          </li>
-          <li>
-            <Link href="/#objects" scroll={false}>
-              objects
-            </Link>
-          </li>
-          <li>
-            <Link href="/#about" scroll={false}>
-              about
-            </Link>
-          </li>
-          <li>
-            <Link href="/#contact" scroll={false}>
-              contact
-            </Link>
-          </li>
-        </ul>
-      </nav>
+    <div id="header" className={styles.header}>
+      <div className="container">
+        <nav>
+          <ul>
+            <li>
+              <Link href="/">home</Link>
+            </li>
+            <li>
+              <Link href="/#projects" scroll={false}>
+                projects
+              </Link>
+            </li>
+            <li>
+              <Link href="/#objects" scroll={false}>
+                objects
+              </Link>
+            </li>
+            <li>
+              <Link href="/#about" scroll={false}>
+                about
+              </Link>
+            </li>
+            <li>
+              <Link href="/#contact" scroll={false}>
+                contact
+              </Link>
+            </li>
+          </ul>
+
+          <ul>
+            <li>
+              <Link href="#">Eng</Link>
+            </li>
+            <li>
+              <Link href="#">Geo</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </div>
   );
 }
