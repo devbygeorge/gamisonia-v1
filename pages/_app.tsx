406a53deb1fe54ac297632749817f94cb7ac05f7
GiorgiPasieshvili <giopasieshvili2@gmail.com>
2023-05-16 19:26:44 +0400

    Move meta information from index to _app page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,39 @@
-import '@/styles/globals.scss'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import "@/styles/globals.scss";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        {/* <!-- HTML Meta Tags --> */}
+        <title>Gamisonia Design</title>
+        <meta name="description" content="Portfolio of an interior designer" />
+        <link rel="icon" href="/images/icon.webp" />
+
+        {/* <!-- Facebook Meta Tags --> */}
+        <meta property="og:url" content="https://gamisonia.com" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Gamisonia Design" />
+        <meta
+          property="og:description"
+          content="Portfolio of an interior designer"
+        />
+        <meta property="og:image" content="/images/meta-image.jpg" />
+
+        {/* <!-- Twitter Meta Tags --> */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="gamisonia.com" />
+        <meta property="twitter:url" content="https://gamisonia.com" />
+        <meta name="twitter:title" content="Gamisonia Design" />
+        <meta
+          name="twitter:description"
+          content="Portfolio of an interior designer"
+        />
+        <meta name="twitter:image" content="/images/meta-image.jpg" />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import { GetStaticProps } from "next";
-import Head from "next/head";
 import { useRouter } from "next/router";
 
 import About from "@/components/About";
@@ -23,34 +22,6 @@ export default function Home({ pageInfo, projects, socials }: Props) {
 
   return (
     <div className="snap">
-      <Head>
-        {/* <!-- HTML Meta Tags --> */}
-        <title>Gamisonia Design</title>
-        <meta name="description" content="Portfolio of an interior designer" />
-        <link rel="icon" href="/images/icon.png" />
-
-        {/* <!-- Facebook Meta Tags --> */}
-        <meta property="og:url" content="https://gamisonia.com" />
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content="Gamisonia Design" />
-        <meta
-          property="og:description"
-          content="Portfolio of an interior designer"
-        />
-        <meta property="og:image" content="/images/meta-image.jpg" />
-
-        {/* <!-- Twitter Meta Tags --> */}
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta property="twitter:domain" content="gamisonia.com" />
-        <meta property="twitter:url" content="https://gamisonia.com" />
-        <meta name="twitter:title" content="Gamisonia Design" />
-        <meta
-          name="twitter:description"
-          content="Portfolio of an interior designer"
-        />
-        <meta name="twitter:image" content="/images/meta-image.jpg" />
-      </Head>
-
       <div className="snap-start">
         <Header translations={translations} />
         <Hero pageInfo={pageInfo} />
