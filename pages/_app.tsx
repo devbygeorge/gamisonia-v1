7afa97413ded481172d7599f5b49b33c42f06297
GiorgiPasieshvili <giopasieshvili2@gmail.com>
2023-05-21 10:11:50 +0400

    Update page meta description

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,17 +7,20 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         {/* <!-- HTML Meta Tags --> */}
-        <title>Gamisonia Design</title>
-        <meta name="description" content="Portfolio of an interior designer" />
+        <title>GAMISONIA Design Studio</title>
+        <meta
+          name="description"
+          content="Discover exceptional interior and furniture design projects by GAMISONIA Design Studio."
+        />
         <link rel="icon" href="/images/icon.svg" />
 
         {/* <!-- Facebook Meta Tags --> */}
         <meta property="og:url" content="https://gamisonia.com" />
         <meta property="og:type" content="website" />
-        <meta property="og:title" content="Gamisonia Design" />
+        <meta property="og:title" content="GAMISONIA Design Studio" />
         <meta
           property="og:description"
-          content="Portfolio of an interior designer"
+          content="Discover exceptional interior and furniture design projects by GAMISONIA Design Studio."
         />
         <meta property="og:image" content="/images/meta-image.jpg" />
 
@@ -25,10 +28,10 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta name="twitter:card" content="summary_large_image" />
         <meta property="twitter:domain" content="gamisonia.com" />
         <meta property="twitter:url" content="https://gamisonia.com" />
-        <meta name="twitter:title" content="Gamisonia Design" />
+        <meta name="twitter:title" content="GAMISONIA Design Studio" />
         <meta
           name="twitter:description"
-          content="Portfolio of an interior designer"
+          content="Discover exceptional interior and furniture design projects by GAMISONIA Design Studio."
         />
         <meta name="twitter:image" content="/images/meta-image.jpg" />
       </Head>
